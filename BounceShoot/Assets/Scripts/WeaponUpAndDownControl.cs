25a63028276bbfccc0d231caa5835c8f2fa10aec
weirdstoies <daniltatarinof@mail.ru>
2018-08-01 03:46:22 +0800

    Настроил нормальное движение орудия

--- BounceShoot/Assets/Scripts/WeaponUpAndDownControl.cs
+++ BounceShoot/Assets/Scripts/WeaponUpAndDownControl.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class WeaponUpAndDownControl : MonoBehaviour {
 
     public float m_WeaponRaisingSpeed = 180f;
+    public float m_WeaponTurningSpeed = 180f;
+    public float m_TopAngelConstraint = -90;
+    public float m_BottomAngelConstraint = 90;
 
     private float m_RaisingValue;
-    private Rigidbody m_RigidBody;
+    private float m_TurningValue;
 
-    private void Awake()
-    {
-        m_RigidBody = GetComponent<Rigidbody>();
+    private void Awake(){
     }
-
-	// Use this for initialization
-	void Start () {		
+    
+	void Start (){		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        m_RaisingValue = -1 * Input.GetAxis("Mouse Y");        
+        m_RaisingValue = -1 * Input.GetAxis("Mouse Y");
+        m_TurningValue = Input.GetAxis("Mouse X");
     }
 
     private void FixedUpdate()
@@ -30,7 +30,25 @@ public class WeaponUpAndDownControl : MonoBehaviour {
 
     private void Raise()
     {
-        float raise = m_RaisingValue * m_WeaponRaisingSpeed * Time.deltaTime;
-        transform.RotateAround(transform.parent.position, transform.parent.right, raise);
+        float raiseValue = m_RaisingValue * m_WeaponRaisingSpeed * Time.deltaTime;
+        float turnValue = m_TurningValue * m_WeaponTurningSpeed * Time.deltaTime;
+        
+        transform.Rotate(raiseValue, 0f, 0f, Space.Self);
+        transform.Rotate(0f, turnValue, 0f, Space.World);
+
+        float xValue = UnityEditor.TransformUtils.GetInspectorRotation(transform).x;
+        if (xValue > m_BottomAngelConstraint || xValue < m_TopAngelConstraint)
+        {
+            xValue = Mathf.Clamp(xValue, m_TopAngelConstraint, m_BottomAngelConstraint);
+            transform.eulerAngles = new Vector3(xValue,
+                                            transform.eulerAngles.y,
+                                            transform.eulerAngles.z);
+        }
+    }
+
+    private bool RisingIsAllowed(float raiseValue, float x)
+    {
+        return (x > 270 && x < 360) || 
+               (x >= 0 && x < 90);
     }
 }
