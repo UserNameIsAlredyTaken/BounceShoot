2a92d25528c91b18f33e88ef2abab6a29583181a
weirdstoies <daniltatarinof@mail.ru>
2018-08-13 16:34:08 +0800

    Научил стрелять

---
+++ BounceShoot/Assets/Scripts/ShellScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellScript : MonoBehaviour {
+
+    public float m_LifeTime = 15;
+
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, m_LifeTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BounceShoot/Assets/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public Rigidbody m_Shell;
+    public Transform m_FireTransform;
+    public AudioSource m_ShootingSource;
+    public AudioClip m_ShootingSound;
+    public float m_ShootingForce = 10;
+    public string m_FireButton = "Fire1";
+
+    private bool m_Fired;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(m_FireButton))
+        {
+            m_ShootingSource.clip = m_ShootingSound;
+            m_ShootingSource.Play();
+            Fire();
+        }
+	}
+
+    private void Fire()
+    {
+        Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+        shellInstance.velocity = m_ShootingForce * m_FireTransform.up;
+    }
+}
